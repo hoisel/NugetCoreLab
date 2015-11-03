d191cac3ae1d4ea442bbb2799287eb5d7ce7e1af
hoisel <danielhoiselferraz@gmail.com>
2015-11-03 14:09:46 -0200

    ....

---
+++ PackagingLab/gulpfile.js
@@ -0,0 +1,24 @@
+﻿/// <binding BeforeBuild='nuget-download' />
+
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    request = require('request'),
+    fs = require('fs');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+
+gulp.task('nuget-download', function (done) {
+    if (fs.existsSync('nuget.exe')) {
+        done();
+        return;
+    }
+
+    request.get('http://nuget.org/nuget.exe')
+        .pipe(plumber())
+        .pipe(fs.createWriteStream('nuget.exe'))
+        .on('close', done);
+});
+
